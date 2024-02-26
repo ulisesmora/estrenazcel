9524ad84f526efb12ab93fc336c64a3477df8f5a
ulisem <ulises.mora@omnidoc.com>
2024-02-26 12:01:37 -0600

    aws ci

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as ngrok from '@ngrok/ngrok';
+// import * as ngrok from '@ngrok/ngrok';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  const listener = await ngrok.forward({ addr: 80, authtoken: '1waXhWDyIZ5gFr7ChbTNNbC8Q8E_2PwNFNNXwCsw5HD3QnbZY' });
+ // const listener = await ngrok.forward({ addr: 80, authtoken: '1waXhWDyIZ5gFr7ChbTNNbC8Q8E_2PwNFNNXwCsw5HD3QnbZY' });
 
   // Output ngrok url to console
-  console.log(`Ingress established at: ${listener.url()}`);
+  //console.log(`Ingress established at: ${listener.url()}`);
   await app.listen(80);
 }
 bootstrap();
