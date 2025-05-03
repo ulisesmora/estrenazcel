de5a5e4391f50fe67b7456be4ce650cabd764039
ulisem <ulisesmora3@gmail.com>
2025-05-03 12:35:34 +0200

    Update create-credit.dto.ts

--- src/credits/dto/create-credit.dto.ts
+++ src/credits/dto/create-credit.dto.ts
@@ -38,13 +38,9 @@ export class CreateCreditDto {
 
 export class PaginationDto {
   @IsOptional()
-  @IsNumber()
-  @Min(1)
   page?: number = 1;
 
   @IsOptional()
-  @IsNumber()
-  @Min(1)
   limit?: number = 10;
 
   @IsOptional()
