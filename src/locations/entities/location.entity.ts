c791af018e2d290d256e6cfc8bfa004615ec5d49
ulisem <ulisesmora3@gmail.com>
2025-08-08 20:02:12 +0200

    Update location.entity.ts

--- src/locations/entities/location.entity.ts
+++ src/locations/entities/location.entity.ts
@@ -1,4 +1,10 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Location extends BaseEntity {
@@ -6,8 +12,10 @@ export class Location extends BaseEntity {
   id: number;
   @Column()
   imei: string;
-  @Column({type: 'float'})
+  @Column({ type: 'float' })
   latitude: number;
-  @Column({type: 'float'})
+  @Column({ type: 'float' })
   longitude: number;
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created: Date;
 }
